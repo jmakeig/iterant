e4a4efb51e3e65cc0a171bb47501772d34cc111f
Justin Makeig <jmpublic+github@makeig.com>
2016-07-24 00:43:42 -0700

    Fixes #8. Refactors type-specific implementations of Iterable into their own modules.
    Implements concat
    
    Copyright
    
    Fixes circular dependencies
    
    Implements Symbol.toStringTag
    
    Fixes concat for Sequences
    
    Creates public getter for hidden _iterable

---
+++ iterable-array.js
@@ -0,0 +1,51 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * Copyright 2016 MarkLogic Corp.                                             *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ 
+ 'use strict';
+
+module.exports = IterableArray;
+const Iterable = require('./iterable');
+
+function IterableArray(array) {
+  if(!Array.isArray(array)) { 
+    throw new TypeError('Can only wrap an Array'); 
+  }
+  if(!this) { return new IterableArray(array); }
+  return Iterable.call(this, array); 
+}
+
+IterableArray.prototype = Object.assign(
+  Object.create(Iterable.prototype), {
+    [Symbol.toStringTag]: 'IterableArray',
+    slice(begin, end) { 
+      return IterableArray(this._iterable.slice(begin, end));
+    },
+    map(mapper, that) {
+      return IterableArray(this._iterable.map(mapper, that));
+    },
+    reduce(reducer, init) {
+      return this._iterable.reduce(reducer, init);
+    },
+    filter(predicate, that) {
+      return IterableArray(this._iterable.filter(predicate, that));
+    },
+    concat(...args) {
+      return IterableArray(this._iterable.concat(...args));
+    },
+    sort(comparator) {
+      return IterableArray(this._iterable.sort(comparator));
+    },
+});
